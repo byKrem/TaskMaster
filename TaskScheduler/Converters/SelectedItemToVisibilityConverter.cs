e97322b7ad0aeee1f5360a9cad7da2d5b69a8177
Rubanov <bykr3m@gmail.com>
2023-04-19 20:20:10 +0300

    Add converter VIsibility for Buttons and ContextMenu

---
+++ TaskScheduler/Converters/SelectedItemToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TaskScheduler.Converters
+{
+    internal class SelectedItemToVisibilityConverter : IValueConverter
+    {
+        public Visibility TrueValue { get; }
+        public Visibility FalseValue { get; }
+
+        public SelectedItemToVisibilityConverter()
+        {
+            TrueValue = Visibility.Visible;
+            FalseValue = Visibility.Collapsed;
+        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
