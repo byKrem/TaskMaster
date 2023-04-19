153955325dddc4dbe490fe64775375188853a456
Rubanov <bykr3m@gmail.com>
2023-04-19 21:56:58 +0300

    Add TaskCreationWindow

---
+++ TaskScheduler/Views/TaskCreationWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TaskScheduler.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskCreationWindow.xaml
+    /// </summary>
+    public partial class TaskCreationWindow : Window
+    {
+        public TaskCreationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
